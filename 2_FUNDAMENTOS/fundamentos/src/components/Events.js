719c539f48c3e0eca5769e0aa084f2d76a72d8f5
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-03-01 19:23:54 -0300

    Entendo eventos

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -1,14 +1,25 @@
 const Events = () => {
     const handleMyEvent = (e) => {
         //alert("Ativou o evento!");
-        console.log(e.pageX);
-        console.clear();
+        console.log(e);
+        console.log("Ativando Evendo com click");
+        //console.clear();
     }
     
     return (
         <div>
             <div>
                 <button onClick={handleMyEvent}>Clique aqui</button>
+            </div>
+            <div>
+                <button onClick={() => console.log("Funções no evento")}>Funções no evento</button>
+            </div>
+            <div>
+                <button onClick={() => {
+                    if(true)
+                        console.log("Não é uma boa pratica");
+                }
+                }>Não é uma boa pratica</button>
             </div>
         </div>
     );
