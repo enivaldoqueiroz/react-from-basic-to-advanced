70ca7568644847c2656a9255024420965c800d09
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-03-02 20:50:30 -0300

    Resolvendo problema da propriedade key

--- 3_AVANCANDO_EM_REACT/avancado/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancado/src/components/ListRender.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ListRender = () => {
-    const [list] = useState(["Matheus", "Pedro", "Josias"]);
+  const [list] = useState(["Matheus", "Pedro", "Josias"]);
+
+  //Correção do Warning: Each child in a list should have a unique "key" prop.
+  const [users] = useState([
+    { id: 1, name: "Matheus", age: 31 },
+    { id: 2, name: "Pedro", age: 31 },
+    { id: 3, name: "Josias", age: 31 },
+  ]);
   return (
     <div>
       <ul>
-        {list.map((item) => (
-            <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+            <li key={user.id}>{user.id} - {user.name} - {user.age}</li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ListRender
+export default ListRender;
