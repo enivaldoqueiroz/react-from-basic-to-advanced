febc4d9f689c20d827f826747b59e86e8e4edf87
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-03-04 20:40:08 -0300

    53. CSS de componentes

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import MyComponent from "./component/MyComponent";
 
 function App() {
   return (
     <div className="App">
+      {/* 52. CSS global */}
       <h1>React com CSS</h1>
+
+      {/* 53. CSS de componentes */}
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
     </div>
   );
 }
