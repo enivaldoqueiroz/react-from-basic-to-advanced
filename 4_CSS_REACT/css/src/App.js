ae5ca6469041cf6eda2f279197b80b7ead4bede2
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-03-04 21:03:11 -0300

    55. CSS inline dinâmico

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,7 +1,10 @@
+import {useState} from 'react'
 import "./App.css";
 import MyComponent from "./component/MyComponent";
 
 function App() {
+  const n = 15;
+  const [name] = useState('Enivaldo')
   return (
     <div className="App">
       {/* 52. CSS global */}
@@ -12,9 +15,28 @@ function App() {
       <p>Este parágrafo é do App.js</p>
 
       {/* 54. CSS inline */}
-      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+      <p
+        style={{
+          color: "blue",
+          padding: "25px",
+          borderTop: "2px solid red",
+        }}
+      >
         Este elemento foi estilizado de forma inline
       </p>
+      {/* 55. CSS inline dinâmico  */}
+      <h2 style={n < 10 ? 
+        { color: "purple" } : { color: "pink" }}>
+        CSS Dinâmico
+      </h2>
+      <h2 style={n > 10 ? 
+        { color: "purple" } : { color: "pink" }}>
+        CSS Dinâmico
+      </h2>
+      <h2 style={name === 'Enivaldo' ? 
+        { color: "purple", backgroundColor: "yellow" } : null}>
+        Enivaldo Queiroz
+      </h2>
     </div>
   );
 }
