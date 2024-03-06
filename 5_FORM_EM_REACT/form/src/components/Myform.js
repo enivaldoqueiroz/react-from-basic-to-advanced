153addda91408d7837f47fd48c410394c4e659eb
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-03-06 19:49:27 -0300

    61. Label envolvendo input

--- 5_FORM_EM_REACT/form/src/components/Myform.js
+++ 5_FORM_EM_REACT/form/src/components/Myform.js
@@ -6,11 +6,17 @@ const Myform = () => {
     <div>
       {/* 60. Criando formulários */}
       <form>
+        {/* 1 - Criação de form */}
         <div>
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder="Digite seu nome" />
-          <input type="submit" value='Enviar'/>
         </div>
+        {/* 2 - Label envolvendo input */}
+        <label>
+          <span>E-mail</span>
+          <input type="email" name="email" placeholder="Digite o seu e-mail"/>
+        </label>
+          <input type="submit" value='Enviar'/>
       </form>
     </div>
   );
